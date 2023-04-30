8db83710af5f4248349faea2214c6c3d7ac4f875
mmoser-blaze <mmoser@perforce.com>
2023-04-30 09:39:34 +0300

    run-in-docker.sh changes
    - add label to container
    - if trying to start - check if not already running
    - if left stopped or not running - kill and prune before starting.

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -39,6 +39,32 @@ EOF
 exit 1
 }
 
+check_docker_engine_running() {
+    assert_bins_in_path "docker"
+    docker ps >/dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echo "Error: docker engine not running"
+        exit 1
+    fi
+}
+
+clean_if_stopped() {
+    STATE=$(docker ps -a --filter 'label=s9k-admin-console'  --format='{{.State}}')
+    if [[ $STATE == "running" ]]; then
+        echo "server is already running"
+        exit 1
+    fi
+    if [[ $STATE != "" ]]; then
+        # force stop and clean up
+        ID=$(docker ps -a --filter 'label=s9k-admin-console'  --format='{{.ID}}')
+        if [[ $ID != "" ]]; then
+            docker kill "$ID"
+            docker container prune -f --filter 'label=docker-php-admin'
+        fi
+    fi
+}
+
+
 SSL="off"
 
 while getopts "htrsvi:p:c:" opt; do
@@ -78,16 +104,19 @@ while getopts "htrsvi:p:c:" opt; do
 done
 
 if [[ $ACTION == 'start' ]]; then
-    
+       
+    check_docker_engine_running
+    clean_if_stopped
+
     if [[ "${SSL}" == "on" ]]; then
-        docker run --rm --name s9k-net -p ${HOST}:${PORT}:${PORT} -v $KUBE_DIR:/kube-mount -e DHOST=$HOST -e DPORT=$PORT -e SSL=on -dt ${IMAGE_LOCATION} 
+        docker run --rm --name s9k-net -p ${HOST}:${PORT}:${PORT} -v $KUBE_DIR:/kube-mount -e DHOST=$HOST -e DPORT=$PORT -e SSL=on -l s9k-admin-console -dt ${IMAGE_LOCATION} 
         PROTO="https"
 
         echo ""
         echo "Note: Runs with self signed certificate"
         echo ""
     else
-        docker run --rm --name s9k-net -p $PORT:$PORT -v $KUBE_DIR:/kube-mount -e DHOST=$HOST -e DPORT=$PORT -dt ${IMAGE_LOCATION} 
+        docker run --rm --name s9k-net -p $PORT:$PORT -v $KUBE_DIR:/kube-mount -e DHOST=$HOST -e DPORT=$PORT -l s9k-admin-console -dt ${IMAGE_LOCATION} 
         PROTO="http"
     fi
     echo "Listen on $PROTO://${HOST}:${PORT}"
